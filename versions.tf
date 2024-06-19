6efe7ef436ea10f5dd9108cbf2ef347f280a733b
moniskhantelus <93679325+moniskhantelus@users.noreply.github.com>
2024-06-19 14:38:24 -0600

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.83, < 6"
+    }
+  }
+
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-network:routes/v9.1.0"
+  }
+}
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.25.0, < 6"
+    }
+  }
+
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-network:subnets/v9.1.0"
+  }
+}
+
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.64, < 6"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.64, < 6"
+    }
+  }
+
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-network:vpc/v9.1.0"
+  }
+  provider_meta "google-beta" {
+    module_name = "blueprints/terraform/terraform-google-network:vpc/v9.1.0"
+  }
+}
